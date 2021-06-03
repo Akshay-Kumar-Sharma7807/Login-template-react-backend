7c27690eb6bd9f8b83c24b71e4b917951d7e927c
Akshay Kumar Sharma <sharmaakshaykumar7807@gmail.com>
2021-06-03 18:08:47 +0530

    Login with Email working

---
+++ routes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const User = require("./user");
+
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err;
+    if (!user) res.send("No User Exists");
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err;
+        res.send("Successfully Authenticated");
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("User Created");
+    }
+  });
+});
+router.get("/user", (req, res) => {
+  // console.log(req);
+  res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
+});
+
+module.exports = router;
